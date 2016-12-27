bb32f41c6020c8206c8c83f674e8451716170c87
stream <penolove15@gmail.com>
2016-12-28 00:42:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# this is a example for transform FDDB ellipse infomation
+# to bbox (xml)
+
+you need to download the files(dataset) of FDDB
+http://vis-www.cs.umass.edu/fddb/
+
+```
+python example.py [ellipseList]
+```
+
+list file were put in FDDB-folds 
+(e.g. FDDB-fold-01-ellipseList.txt)
+there are 01~10 ellipselist
+
+the Annotations and JPGE will be fill with files
+(adding new by index)
+
+
+
+
---
+++ example.py
@@ -0,0 +1,110 @@
+
+from lxml import etree
+import sys
+import cv2
+import math
+import glob
+
+cur_ind=0
+outfileID=len(glob.glob("JPEGImages/*.jpg"))
+
+
+def img2xml(path,objects,shape):
+    root = etree.Element("annotation")
+    folder = etree.SubElement(root, "folder")
+    filename = etree.SubElement(root, "filename")
+    source = etree.SubElement(root, "source")
+    databases = etree.SubElement(source, "database")
+
+    folder.text = "VOC2007"
+    filename.text = str(path).zfill(6)
+    databases.text = "FDDB"
+
+    size = etree.SubElement(root, "size")
+    width = etree.SubElement(size,"width")
+    height = etree.SubElement(size,"height")
+    depth = etree.SubElement(size,"depth")
+    depth.text = str(shape[2])
+    width.text = str(shape[1])
+    height.text = str(shape[0])
+
+    obj_count=0
+    for obj in objects:
+        #object
+        obj=[float(i) for i in obj.split()]
+        #the smallest circumscribed parallelogram
+        #[link] https://github.com/nouiz/lisa_emotiw/blob/master/emotiw/common/datasets/faces/FDDB.py
+        maj_rad = obj[0]
+        min_rad = obj[1]
+        angle = obj[2]
+        xcenter = obj[3]
+        ycenter = obj[4]
+        cosin = math.cos(math.radians(-angle))
+        sin = math.sin(math.radians(-angle))
+
+        x1 = cosin * (-min_rad) - sin * (-maj_rad) + xcenter
+        y1 = sin * (-min_rad) + cosin * (-maj_rad) + ycenter
+        x2 = cosin * (min_rad) - sin * (-maj_rad) + xcenter
+        y2 = sin * (min_rad) + cosin * (-maj_rad) + ycenter
+        x3 = cosin * (min_rad) - sin * (maj_rad) + xcenter
+        y3 = sin * (min_rad) + cosin * (maj_rad) + ycenter
+        x4 = cosin * (-min_rad) - sin * (maj_rad) + xcenter
+        y4 = sin * (-min_rad) + cosin * (maj_rad) + ycenter
+        wid=[x1,x2,x3,x4]
+        hei=[y1,y2,y3,y4]
+        xmin_ = int(min(wid))
+        xmax_ = int(max(wid))
+        ymin_ = int(min(hei))
+        ymax_ = int(max(hei))
+        
+        # check if out of box
+        if(xmin_ >0 and ymin_>0 and xmax_<shape[1] and ymax_<shape[0]):
+            obj_count+=1
+            object_=etree.SubElement(root, "object")
+            name=etree.SubElement(object_, "name")
+            name.text="face"
+            pose=etree.SubElement(object_, "pose")
+            pose.text="Unspecified"
+            truncated=etree.SubElement(object_, "truncated")
+            truncated.text="0"
+            difficult=etree.SubElement(object_, "difficult")
+            difficult.text="0"
+            # bndbox
+            bndbox=etree.SubElement(object_, "bndbox")
+            xmin=etree.SubElement(bndbox,"xmin")
+            ymin=etree.SubElement(bndbox,"ymin")
+            xmax=etree.SubElement(bndbox,"xmax")
+            ymax=etree.SubElement(bndbox,"ymax")
+            xmin.text = str(xmin_)
+            ymin.text = str(ymin_)
+            xmax.text = str(xmax_)
+            ymax.text = str(ymax_)
+    if obj_count>0:
+        et = etree.ElementTree(root)
+        et.write("Annotations/"+path+".xml", pretty_print=True)
+
+if __name__=="__main__":
+    # you need to modify the path_img below
+    # and the FDDB-fold-were assign by your own
+    elliFold='FDDB-folds/'
+    if len(sys.argv) < 2:
+        ellipseList='FDDB-fold-01-ellipseList.txt'
+    elif len(sys.argv)==2:
+        ellipseList=sys.argv[1]
+    else:
+        print "usage : python example.py [ellipseList]"
+    current_file=open(elliFold+ellipseList,'r')
+    image_with_target=[i.replace('\n','') for i in current_file.readlines()]
+    while (cur_ind<len(image_with_target)):
+        path_img = '../'+image_with_target[cur_ind]+'.jpg'
+        img = cv2.imread(path_img) 
+        cur_ind+=1
+        len_obj=int(image_with_target[cur_ind])
+        cur_ind+=1
+        objects=image_with_target[cur_ind:cur_ind+len_obj]
+        cur_ind+=len_obj
+        path=str(outfileID).zfill(6)
+        img2xml(path,objects,img.shape)
+        cv2.imwrite("JPEGImages/"+path+".jpg", img)
+        outfileID+=1
+
