dcde9577bb7e86876e9d1f4dbc1443cafa4cb32c
stream <penolove15@gmail.com>
2017-01-03 11:32:22 +0800

    add image and edit readme

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 you need to download the files(dataset) of FDDB
 http://vis-www.cs.umass.edu/fddb/
 
+Translate ellipseList to annotation xml files:
+
 ```
-python example.py [ellipseList]
+python anno2xml.py [ellipseList]
 ```
 
 list file were put in FDDB-folds 
@@ -15,6 +17,19 @@ there are 01~10 ellipselist
 the Annotations and JPGE will be fill with files
 (adding new by index)
 
+the avg w,h for FDDB face is:
+(97.4,144.6)
 
+          w         h
+1 198.41098 296.33902
+2 127.03337 189.59485
+3  37.87374  54.05429
+4  80.88835 120.00979
+| w         | h           | 
+| ------------- |:-------------:| 
+| 37.8       | 54.05 | 
+| 80.8      | 120.0      | 
+| 127.0 | 189.5      | 
+| 198.41 | 296.33      | 
 
-
+![alt tag](https://raw.githubusercontent.com/penolove/pyxml_wider/master/WiderfaceWH.png)
--- wh_visual.R
+++ wh_visual.R
@@ -1,6 +1,7 @@
 library(hexbin)
 library(RColorBrewer)
 library(MASS)
+#w=read.csv('whoutput.csv', header=FALSE)
 w=read.csv('output.csv', header=FALSE)
 
 df <- data.frame(w)
@@ -11,10 +12,10 @@ rf <- colorRampPalette(rev(brewer.pal(11,'Spectral')))
 plot(h, colramp=rf)
 
 
-h1 <- hist(df$V1, breaks=25, plot=F)
-h2 <- hist(df$V2, breaks=25, plot=F)
+h1 <- hist(df$V1, breaks=50, plot=F)
+h2 <- hist(df$V2, breaks=50, plot=F)
 top <- max(h1$counts, h2$counts)
-k <- kde2d(df$V1, df$V2, n=25)
+k <- kde2d(df$V1, df$V2, n=50)
 r <- rf(32)
 
 # margins
@@ -29,3 +30,19 @@ barplot(h2$counts, axes=F, xlim=c(0, top), space=0, col='red', horiz=T)
 
 mean(df$V1)
 mean(df$V2)
+
+# k-means
+
+library(cluster)
+k.max=15
+wss <- sapply(1:k.max, 
+              function(k){kmeans(w, k, nstart=10 )$tot.withinss})
+plot(1:k.max, wss,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares")
+abline(v = 4, lty =2)
+
+
+op=kmeans(w, 4, nstart=10 )
+op$centers
